6aace07dfc2f583e0e84bfce56f427dd83ee2be5
larissaliper <larissaliper3@gmail.com>
2025-02-14 11:40:38 -0300

    14/02

--- index.php
+++ index.php
@@ -5,39 +5,52 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    
-
 </head>
 <body>
-
-    <div clss="container-fluid bg-primary text-white text-center p-5">
-    <h1>Projeto SW2</h1> 
-    <p>
-        Elaborado por Larissa Liper
-    </p>
+    <div class="container-fluid bg-primary text-white text-center p-5">
+        <h1>Projeto SW2</h1>
+        <p>
+            Elaborado por Larissa Liper
+        </p>
     </div>
 
     <div class="container">
         <div class="row">
 
+            <div class="col-sm-4">
+                <h1 class="bg-primary text-warning">Administrador</h1>
+                <p class="test-danger">
+                    Utilize esta opção para realizar o cadastro de novos administradores, ou edita-los.
+                </p>
+
+            </div>
+
+            <div class="col-sm-4">
+                <h1 class="bg-danger">Categorias</h1>
+                <p>
+                    Utilize esta opção para realizar o cadastrar as categorias dos produtos que serão vendidos on-line.
+                </p>
+            </div>
+
+            <div class="col-sm-4">
+                <h1 class="bg-success">Fornecedor</h1>
+                <p>
+                    Utilize esta opção para cadastrar os fornecedores dos produtos que serão vendidos on-line.
+                </p>
+            </div>
+
         </div>
 
-        <div class="col-sm-4">
-            <h1>Administrador</h1>
-
+        <div class="row">
+            <div class="col-sm-6">
+                <h1>Produtos</h1>
         </div>
-
-        <div class="col-sm-4">
-            <h1>Forneceor</h1>
-            
+        <div class="col-sm-6">
+            <h1>Vendas</h1>
         </div>
-    
-    
     </div>
-
-
+</div>
 </body>
 </html>
