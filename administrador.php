757c38bb478ffdb2271d78f0e1088d69d4022f67
larissaliper <larissaliper3@gmail.com>
2025-03-18 11:00:17 -0300

    aula18/3
    18/3

--- administrador.php
+++ administrador.php
@@ -5,26 +5,33 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Cadastro de Categorias</title>
+    <title>Cadastro de Admin</title>
 </head>
 <body>
     
-    <div class="bg-danger text-white p-3 text-center">
-        <h1>Cadastro de Categorias</h1>
+    <div class="bg-primary text-white p-3 text-center">
+        <h1>Cadastro de Administradores</h1>
     </div>
 
     <div class="container">
         <div class="row">
-            <div class="col-sm-8 mx-auto mt-3 border border-danger">
+            <div class="col-sm-8 mx-auto mt-3 border border-primary">
                 <h3 class="text-center">Preencha os dados abaixo</h3>
 
-                <form action="#">
+                <form action="administrador_cadastro.php">
 
                     <p>
-                            Digite a Categoria<br>
-                            <input type="text" name="categoria" class="form-control" />
+                            Digite o nome do administrador<br>
+                            <input type="text" name="nome" class="form-control" />
                     </p>
-                     
+                     <p>
+                        Digite o login<br>
+                        <input type="text" name="login" class="form-control"/>
+                     </p>
+                     <p>
+                    Digite a senha<br>
+                        <input type="password" name="senha" class="form-control" />
+                       </p>
 
                        <p>
                             <input type="submit" value="Cadastrar" class="btn btn-success" />
--- index.php
+++ index.php
@@ -3,41 +3,41 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Felipe</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="container-fluid bg-primary text-white text-center p-5">
+    <div class="container-fluid bg-dark text-white text-center p-5">
         <h1>Projeto SW2</h1>
         <p>
             Elaborado por Larissa Liper
         </p>
     </div>
 
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
 
             <div class="col-sm-4">
-                <h1 class="bg-primary text-warning">Administrador</h1>
-                <p class="test-danger">
-                    Utilize esta opção para realizar o cadastro de novos administradores, ou edita-los.
-                </p>
-
-            </div>
-
-            <div class="col-sm-4">
-                <h1 class="bg-danger">Categorias</h1>
-                <p>
-                    Utilize esta opção para realizar o cadastrar as categorias dos produtos que serão vendidos on-line.
+                <a href='administrador.php' style="text-decoration : none"><h1 class="bg-primary text-warning">Administrador</h1></a>
+                <p class="text-primary">
+                    Utilize essa opção para realizar o cadastro de novos administradores, ou edita-los.
                 </p>
             </div>
 
             <div class="col-sm-4">
-                <h1 class="bg-success">Fornecedor</h1>
-                <p>
-                    Utilize esta opção para cadastrar os fornecedores dos produtos que serão vendidos on-line.
+                <a href='categoria.php' style="text-decoration : none"><h1 class="bg-danger text-white">Categorias</h1></a>
+                <p class="text-danger">
+                    Utilize essa opção para realizar o cadastro das categorias do produtos que serão vendidos online, ou edita-las.
+                </p>
+            </div>
+                
+
+            <div class="col-sm-4">
+                <a href='fornecedor.php'  style="text-decoration : none"><h1 class="bg-success text-danger">Fornecedor</h1></a>
+                <p class="text-success">
+                    Utilize essa opção para realizar o cadastro dos fornecedores dos produtos que serão vendidos online, ou edita-las.
                 </p>
             </div>
 
@@ -45,10 +45,12 @@
 
         <div class="row">
             <div class="col-sm-6">
-                <h1>Produtos</h1>
+                <a href='produto.php'  style="text-decoration : none"><h1 class="bg-warning">Produtos</h1></a>
+                <p class="text-warning">Use essa opção para cadastrar os produtos que serão vendidos no site</p>
         </div>
         <div class="col-sm-6">
-            <h1>Vendas</h1>
+            <h1 class="bg-secondary">Vendas</h1>
+            <p class="text-secondary">Use essa opção para consultar as vendas realizadas pelos clientes</p>
         </div>
     </div>
 </div>
